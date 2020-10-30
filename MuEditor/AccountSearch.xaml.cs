12b5e287a5e7fbb769e2a554ae6aa977b5b45197
SanoranX <ilyarafailoiv32@gmail.com>
2020-10-30 18:17:35 +0300

    Added files that were not included

---
+++ MuEditor/AccountSearch.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MuEditor
+{
+    /// <summary>
+    /// Логика взаимодействия для AccountSearch.xaml
+    /// </summary>
+    public partial class AccountSearch : Window
+    {
+        public AccountSearch()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
